c00f0669a6a64fe2f43f5c4f940332f32e95053f
mrmatthews88 <self@mrmatthews.me.uk>
2019-08-20 21:43:48 +0100

    innit

---
+++ src/views/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel v-for="(item, i) in listArray" :key="i">
+      <v-expansion-panel-header>{{ item.title }}</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        {{ item.content }}
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      listArray: [
+        { title: "this is a title", content: "aaaaa" },
+        { title: "this is a title", content: "aaaaa" },
+        { title: "this is a title", content: "aaaaa" },
+        { title: "this is a title", content: "aaaaa" },
+        { title: "this is a title", content: "aaaaa" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
